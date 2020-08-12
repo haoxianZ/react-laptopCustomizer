04e4aae3a1186f1b5f747df7a40dd0fbf6fba784
John Zhang <johnhaoxian@gmail.com>
2020-08-12 12:27:48 -0400

    thrid commit

--- src/Feature.js
+++ src/Feature.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-
+import slugify from 'slugify';
+import FeatureItem from './FeatureItem';
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
 class Feature extends Component{
+  
     render(){
+      const featureHash = this.props.feature + '-' + this.props.idx;
+  const options = this.props.features[this.props.feature].map(item => {
+  const itemHash = slugify(JSON.stringify(item));
+  return(
+      <div key={itemHash} className="feature__item">
+      <FeatureItem itemHash={itemHash}
+      key={itemHash}
+  name={slugify(this.props.feature)}
+  checked={item.name === this.props.selected[this.props.feature].name}
+  updateFeature={this.props.updateFeature}
+  Display={item.name} cost={USCurrencyFormat.format(item.cost)}
+  feature={this.props.feature} item={item}/>
+
+  
+      </div>
+      
+
+  )
+  
+  });
         return(
-            <fieldset className="feature" key={this.props.featureHash}>
+            <fieldset className="feature" key={featureHash}>
           <legend className="feature__name">
             <h3>{this.props.feature}</h3>
           </legend>
-          {this.props.options}
+          {options}
         </fieldset>
         )
     }
--- src/SumFeatures.js
+++ src/SumFeatures.js
@@ -3,40 +3,16 @@ import slugify from 'slugify';
 import FeatureItem from './FeatureItem';
 import Feature from './Feature';
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+
 
 class SumFeatures extends Component{
     render(){
         return (
-            Object.keys(this.props.features).map((feature, idx) => {
-                    const featureHash = feature + '-' + idx;
-                    const options = this.props.features[feature].map(item => {
-                    const itemHash = slugify(JSON.stringify(item));
-                    return(
-                        <div key={itemHash} className="feature__item">
-                        <FeatureItem itemHash={itemHash}
-                        key={itemHash}
-                    name={slugify(feature)}
-                    checked={item.name === this.props.selected[feature].name}
-                    updateFeature={this.props.updateFeature}
-                    Display={item.name} cost={USCurrencyFormat.format(item.cost)}
-                    feature={feature} item={item}/>
-            
-                    
-                        </div>
-                        
-            
-                    )
-                    
-                    });
-                    
-                    return (
-                    <Feature key={idx} feature={feature} options={options} featureHash={featureHash}/>
-                    );
-                })
+            Object.keys(this.props.features).map((feature, idx) => <Feature 
+            features={this.props.features} 
+            updateFeature={this.props.updateFeature}
+            selected = {this.props.selected}
+            feature={feature} idx={idx}/>)
         )
         
     
